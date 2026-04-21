c13ce45b714408fe73ef40763543f5e62e2df490
B4tyrkhan <darnix79@gmail.com>
2026-04-21 15:02:24 +0500

    Added Tabs Navigation

--- main.py
+++ main.py
@@ -1,9 +1,134 @@
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty, ListProperty
+
 from kivymd.app import MDApp
-from kivymd.uix.label import MDLabel
+from kivymd.theming import ThemableBehavior
+from kivymd.uix.list import OneLineIconListItem, MDList
+from kivymd.uix.tab import MDTabsBase
+from kivymd.uix.floatlayout import MDFloatLayout
+
+KV = '''
+<ItemDrawer>:
+    theme_text_color: "Custom"
+    on_release: self.parent.set_color_item(self)
+
+    IconLeftWidget:
+        icon: root.icon
+        theme_text_color: "Custom"
+        text_color: root.text_color
+
+
+<ContentNavigationDrawer>:
+    orientation: "vertical"
+    padding: "8dp"
+    spacing: "8dp"
+
+    AnchorLayout:
+        anchor_x: "left"
+        size_hint_y: None
+        height: avatar.height
+
+        Image:
+            id: avatar
+            size_hint: None, None
+            size: "56dp", "56dp"
+            source: "data/logo/logo3-min.png"
+
+    MDLabel:
+        text: "KivyMD library"
+        font_style: "Button"
+        size_hint_y: None
+        height: self.texture_size[1]
+
+    MDLabel:
+        text: "kivydevelopment@gmail.com"
+        font_style: "Caption"
+        size_hint_y: None
+        height: self.texture_size[1]
+
+    ScrollView:
+        DrawerList:
+            id: md_list
+
+
+<Tab>:
+    MDLabel:
+        text: root.title
+        halign: "center"
+
+
+Screen:
+    MDNavigationLayout:
+
+        ScreenManager:
+            Screen:
+
+                BoxLayout:
+                    orientation: "vertical"
+
+                    MDToolbar:
+                        title: "Mortgage Calculator"
+                        elevation: 10
+                        left_action_items: [['menu', lambda x: nav_drawer.set_state("open")]]
+
+                    MDTabs:
+                        id: tabs
+
+        MDNavigationDrawer:
+            id: nav_drawer
+
+            ContentNavigationDrawer:
+                id: content_drawer
+'''
+
+
+class Tab(MDFloatLayout, MDTabsBase):
+    title = StringProperty("")
+
+
+class ContentNavigationDrawer(BoxLayout):
+    pass
+
+
+class ItemDrawer(OneLineIconListItem):
+    icon = StringProperty()
+    text_color = ListProperty([0, 0, 0, 1])
+
+
+class DrawerList(ThemableBehavior, MDList):
+    def set_color_item(self, instance_item):
+        for item in self.children:
+            if item.text_color == self.theme_cls.primary_color:
+                item.text_color = self.theme_cls.text_color
+        instance_item.text_color = self.theme_cls.primary_color
 
 
 class MortgageCalculatorApp(MDApp):
     def build(self):
-        return MDLabel(text="Hello, World", halign="center")
+        self.theme_cls.primary_palette = "Orange"
+        self.theme_cls.theme_style = "Light"
+        return Builder.load_string(KV)
+
+    def on_start(self):
+        icons_item = {
+            "folder": "My files",
+            "account-multiple": "Shared with me",
+            "star": "Starred",
+            "history": "Recent",
+            "checkbox-marked": "Checked",
+            "upload": "Upload",
+        }
+
+        for icon_name, item_text in icons_item.items():
+            self.root.ids.content_drawer.ids.md_list.add_widget(
+                ItemDrawer(icon=icon_name, text=item_text)
+            )
+
+        for icon_name, name_tab in icons_item.items():
+            self.root.ids.tabs.add_widget(
+                Tab(icon=icon_name, title=name_tab)
+            )
+
 
 MortgageCalculatorApp().run()
